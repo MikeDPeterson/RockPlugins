ee1c1d82eb3ffb984a6cf64681e26ace421eb74e
mikepetersonccv <MikePeterson@ccvonline.com>
2013-09-27 02:37:51 -0700

    started on markdown block

---
+++ RockPOS/RockWeb/Plugins/com_rockMP/Cms/MarkdownContent.ascx
@@ -0,0 +1,18 @@
+﻿<%@ Control Language="C#" AutoEventWireup="true" CodeFile="MarkdownContent.ascx.cs" Inherits="RockWeb.Plugins.com_rockMP.Cms.MarkdownContent" %>
+
+<asp:UpdatePanel ID="upPanel" runat="server">
+    <ContentTemplate>
+        <asp:Button ID="btnRestart" runat="server" CssClass="btn btn-danger" Text="Restart WebApp" OnClick="btnRestart_Click" />
+        <div class="row">
+            <Rock:RockTextBox ID="tbMarkdownInput" runat="server" Label="Edit Markdown" Rows="20" Font-Names="Courier-New" TextMode="MultiLine" CssClass="col-md-6" />
+            <div class="col-md-6"><asp:Literal ID="lCheatSheet" runat="server" Text="Here is the cheatsheet..." /></div>
+        </div>
+        <asp:Button ID="btnTransform" runat="server" OnClick="btnTransform_Click" CssClass="btn" Text="Transform" />
+        <asp:Button ID="Button1" runat="server" OnClick="btnTransform_Click" CssClass="btn" Text="Transform" />
+        <asp:Button ID="Button3" runat="server" OnClick="btnTransform_Click" CssClass="btn" Text="Transform" />
+        <asp:Button ID="Button2" runat="server" OnClick="btnTransform_Click" CssClass="btn" Text="Transform" />
+        <div class="jumbotron">
+            <asp:Literal ID="lHtml" runat="server" />
+        </div>
+    </ContentTemplate>
+</asp:UpdatePanel>
---
+++ RockPOS/RockWeb/Plugins/com_rockMP/Cms/MarkdownContent.ascx.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Rock.Web.UI;
+using MarkdownSharp;
+using System.Net;
+using Rock.Model;
+using System.Web.Compilation;
+using System.Web.Hosting;
+
+namespace RockWeb.Plugins.com_rockMP.Cms
+{
+    public partial class MarkdownContent : RockBlock
+    {
+        protected void btnTransform_Click( object sender, EventArgs e )
+        {
+            MarkdownSharp.Markdown markdown = new Markdown();
+            lHtml.Text = markdown.Transform( tbMarkdownInput.Text );
+        }
+        
+        protected void btnRestart_Click( object sender, EventArgs e )
+        {
+            
+            BuildManager.GetCompiledType(this.CurrentBlock.BlockType.Path);
+            
+            
+            
+            System.Web.Hosting.HostingEnvironment.InitiateShutdown();
+            
+            Rock.Web.Cache.PageCache.Flush( this.CurrentPage.Id );
+            Rock.Web.Cache.BlockTypeCache.Flush( this.CurrentBlock.BlockTypeId );
+            Rock.Web.Cache.BlockCache.Flush( this.CurrentBlock.Id );
+            WebRequest request = WebRequest.Create( Request.Url );
+            WebResponse response = request.GetResponse();
+        }
+}
+}
